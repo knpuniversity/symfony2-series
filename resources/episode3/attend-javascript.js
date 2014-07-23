30be3cdeb565e9097bf568fcce874b2a045613e8
Ryan Weaver <ryan@thatsquality.com>
2014-07-23 10:32:02 -0500

    Adding episode3 JS resource for attend AJAX

---
+++ resources/episode3/attend-javascript.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.js-attend-toggle').on('click', function(e) {
+        // prevents the browser from "following" the link
+        e.preventDefault();
+
+        var $anchor = $(this);
+        var url = $(this).attr('href')+'.json';
+
+        $.post(url, null, function(data) {
+            if (data.attending) {
+                var message = 'See you there!';
+            } else {
+                var message = 'We\'ll miss you!';
+            }
+
+            $anchor.after('<span class="label label-default">&#10004; '+message+'</span>');
+            $anchor.hide();
+        });
+
+        return false;
+    });
+});
