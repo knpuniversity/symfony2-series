4d154713c42b7069582cfedea224587dbc3fecf3
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:19 -0500

    [tuts-apply-diff] Applying existing diff file for step login-form-create-user-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Yoda\EventBundle\EventBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Yoda\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
