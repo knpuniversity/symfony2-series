e4e9abaa7a29f5cd7af4a2c34af5bd9301f50ac7
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:25 -0500

    [tuts-apply-diff] Applying existing diff file for step save-user-create-user-but-no-password

--- src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
+++ src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -4,11 +4,19 @@ namespace Yoda\UserBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Yoda\UserBundle\Entity\User;
 
 class LoadUsers implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $user = new User();
+        $user->setUsername('darth');
+        // todo - fill in this encoded password... ya know... somehow...
+        $user->setPassword('');
+        $manager->persist($user);
 
+        // the queries aren't done until now
+        $manager->flush();
     }
 }
