e21ec8b136576df7dfa79f62c0035ba056232998
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:10 -0500

    [tuts-apply-diff] Applying existing diff file for step routing-switch-argument-order

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($firstName, $count)
+    public function indexAction($count, $firstName)
     {
         var_dump($firstName, $count);die;
 
