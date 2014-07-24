35b661498a204694a064c9bc644769e1e3a9884b
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:10 -0500

    [tuts-apply-diff] Applying existing diff file for step routing-add-count-argument-name

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,8 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($firstName)
+    public function indexAction($firstName, $count)
     {
+        var_dump($firstName, $count);die;
+
         return $this->render(
             'EventBundle:Default:index.html.twig',
             array('name' => $firstName)
