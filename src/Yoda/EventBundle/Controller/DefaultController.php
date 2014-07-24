69ff8a425ce22e5a1fcce83b5647cdab895be312
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:11 -0500

    [tuts-apply-diff] Applying existing diff file for step controller-use-renderresponse

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -11,11 +10,9 @@ class DefaultController extends Controller
     {
         $templating = $this->container->get('templating');
 
-        $content = $templating->render(
+        return $templating->renderResponse(
             'EventBundle:Default:index.html.twig',
             array('name' => $firstName)
         );
-
-        return new Response($content);
     }
 }
