747f36eaeae16a3c0f543c47d658be590f441cb5
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:36 -0500

    [tuts-apply-diff] Applying existing diff file for step form-class-filling-in-the-guts

--- src/Yoda/UserBundle/Form/RegisterFormType.php
+++ src/Yoda/UserBundle/Form/RegisterFormType.php
@@ -3,9 +3,29 @@
 namespace Yoda\UserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class RegisterFormType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', 'text')
+            ->add('email', 'email')
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+            ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Yoda\UserBundle\Entity\User',
+        ));
+    }
+
     public function getName()
     {
         return 'user_register';
