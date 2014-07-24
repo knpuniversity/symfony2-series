d74611f6955d9b10a30232ab5aad50ea218f7556
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:50:06 -0500

    [tuts-apply-diff] Applying existing diff file for step doctrinelistener-add-handleevent-function

--- src/Yoda/UserBundle/Doctrine/UserListener.php
+++ src/Yoda/UserBundle/Doctrine/UserListener.php
@@ -2,10 +2,24 @@
 
 namespace Yoda\UserBundle\Doctrine;
 
+use Yoda\UserBundle\Entity\User;
+
 class UserListener
 {
     public function prePersist()
     {
         die('Something is being inserted!');
+    }
+
+    private function handleEvent(User $user)
+    {
+        $plainPassword = $user->getPlainPassword();
+
+        $encoder = $this->container->get('security.encoder_factory')
+            ->getEncoder($user)
+        ;
+
+        $password = $encoder->encodePassword($plainPassword, $user->getSalt());
+        $user->setPassword($password);
     }
 }
