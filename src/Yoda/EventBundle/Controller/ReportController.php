d497ae636c62fc05e27fe8b2e0fec5185f374ad4
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:59 -0500

    [tuts-apply-diff] Applying existing diff file for step csv-basic-csv-endpoint

--- src/Yoda/EventBundle/Controller/ReportController.php
+++ src/Yoda/EventBundle/Controller/ReportController.php
@@ -3,6 +3,7 @@
 namespace Yoda\EventBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReportController extends Controller
 {
@@ -11,6 +12,21 @@ class ReportController extends Controller
      */
     public function updatedEventsAction()
     {
+        $em = $this->getDoctrine()->getManager();
 
+        $events = $em->getRepository('EventBundle:Event')
+            ->getRecentlyUpdatedEvents();
+
+        $rows = array();
+        foreach ($events as $event) {
+            $data = array($event->getId(), $event->getName(), $event->getTime()->format('Y-m-d H:i:s'));
+
+            $rows[] = implode(',', $data);
+        }
+
+        $content = implode("\n", $rows);
+        $response = new Response($content);
+
+        return $response;
     }
 }
