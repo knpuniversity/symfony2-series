e9dfea6b0373ef2e9abfc702c1374014d3c9832d
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:10 -0500

    [tuts-apply-diff] Applying existing diff file for step controller-returning-direct-response

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,14 +3,12 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($count, $firstName)
     {
-        return $this->render(
-            'EventBundle:Default:index.html.twig',
-            array('name' => $firstName)
-        );
+        return new Response('It\'s a traaaaaaaap!');
     }
 }
