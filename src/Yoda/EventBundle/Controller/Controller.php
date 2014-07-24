cdef295979f3b0121341f3ca7ef5502f9b510a13
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:48 -0500

    [tuts-apply-diff] Applying existing diff file for step base-controller-move-enforceownersecurity

--- src/Yoda/EventBundle/Controller/Controller.php
+++ src/Yoda/EventBundle/Controller/Controller.php
@@ -3,6 +3,8 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+use Yoda\EventBundle\Entity\Event;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class Controller extends BaseController
 {
@@ -10,4 +12,15 @@ class Controller extends BaseController
     {
         return $this->container->get('security.context');
     }
-} 
+
+    public function enforceOwnerSecurity(Event $event)
+    {
+        $user = $this->getUser();
+
+        if ($user != $event->getOwner()) {
+            // if you're using 2.5 or higher
+            // throw $this->createAccessDeniedException('You are not the owner!!!');
+            throw new AccessDeniedException('You are not the owner!!!');
+        }
+    }
+}
