dba7432c8d8a2e13d75d057833731a23f1ed8f25
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:25 -0500

    [tuts-apply-diff] Applying existing diff file for step save-user-implement-containerawareinterface-on-fixture

--- src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
+++ src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -4,10 +4,17 @@ namespace Yoda\UserBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Yoda\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadUsers implements FixtureInterface
+class LoadUsers implements FixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
@@ -18,5 +25,10 @@ class LoadUsers implements FixtureInterface
 
         // the queries aren't done until now
         $manager->flush();
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 }
