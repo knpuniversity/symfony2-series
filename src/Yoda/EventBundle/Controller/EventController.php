2870315d849c15d01d4b64e5552144867ba5c966
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:53 -0500

    [tuts-apply-diff] Applying existing diff file for step manytomany-adding-attendaction-logic

--- src/Yoda/EventBundle/Controller/EventController.php
+++ src/Yoda/EventBundle/Controller/EventController.php
@@ -236,7 +236,22 @@ class EventController extends Controller
 
     public function attendAction($id)
     {
+        $em = $this->getDoctrine()->getManager();
+        /** @var $event \Yoda\EventBundle\Entity\Event */
+        $event = $em->getRepository('EventBundle:Event')->find($id);
 
+        if (!$event) {
+            throw $this->createNotFoundException('No event found for id '.$id);
+        }
+
+        $event->getAttendees()->add($this->getUser());
+
+        $em->persist($event);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('event_show', array(
+            'slug' => $event->getSlug()
+        )));
     }
 
     public function unattendAction($id)
