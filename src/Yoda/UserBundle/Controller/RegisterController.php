8843a551c057251e60a582263329dc625de0306a
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:30 -0500

    [tuts-apply-diff] Applying existing diff file for step registration-create-controller-class

---
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yoda\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RegisterController extends Controller
+{
+    public function registerAction()
+    {
+        // todo
+    }
+}
