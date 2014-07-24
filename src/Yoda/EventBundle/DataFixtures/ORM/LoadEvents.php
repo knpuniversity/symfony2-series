74798dd6290ff44e89073c0e5bcf98767b9bb167
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:16 -0500

    [tuts-apply-diff] Applying existing diff file for step fixtures-create-new-fixtures-file

---
+++ src/Yoda/EventBundle/DataFixtures/ORM/LoadEvents.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Yoda\EventBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadEvents implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        // ... todo
+    }
+}
