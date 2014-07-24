4d2dbb09594595f191f3093e172f4872e7983dbd
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:31 -0500

    [tuts-apply-diff] Applying existing diff file for step registration-adding-template-use

--- src/Yoda/UserBundle/Controller/RegisterController.php
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -4,6 +4,7 @@ namespace Yoda\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class RegisterController extends Controller
 {
