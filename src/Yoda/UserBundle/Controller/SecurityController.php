a641935b45e5c384309fa33ee31bb9081351552f
Ryan Weaver <ryan@thatsquality.com>
2014-07-24 15:49:19 -0500

    [tuts-apply-diff] Applying existing diff file for step login-form-annotation-routing

--- src/Yoda/UserBundle/Controller/SecurityController.php
+++ src/Yoda/UserBundle/Controller/SecurityController.php
@@ -3,9 +3,13 @@
 namespace Yoda\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class SecurityController extends Controller
 {
+    /**
+     * @Route("/login", name="login_form")
+     */
     public function loginAction()
     {
     }
