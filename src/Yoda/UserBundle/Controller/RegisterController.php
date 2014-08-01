43c7290b7b1d6406b921c287684aa01552d0687e
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:38:21 -0400

    [tuts-apply-diff] Applying existing diff file for step form-submit-basic-submit-code

--- src/Yoda/UserBundle/Controller/RegisterController.php
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -5,6 +5,7 @@ namespace Yoda\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegisterController extends Controller
 {
@@ -12,7 +13,7 @@ class RegisterController extends Controller
      * @Route("/register", name="user_register")
      * @Template
      */
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $form = $this->createFormBuilder()
             ->add('username', 'text')
@@ -22,6 +23,12 @@ class RegisterController extends Controller
             ))
             ->getForm()
         ;
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            var_dump($form->getData());die;
+        }
 
         return array('form' => $form->createView());
     }
