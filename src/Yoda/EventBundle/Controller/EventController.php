b6476fc690aef1f1c72b9fe59e2ed3ffb005660d
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:39:29 -0400

    [tuts-apply-diff] Applying existing diff file for step user-controller-getuser

--- src/Yoda/EventBundle/Controller/EventController.php
+++ src/Yoda/EventBundle/Controller/EventController.php
@@ -25,11 +25,7 @@ class EventController extends Controller
      */
     public function indexAction()
     {
-        $user = $this->container
-            ->get('security.context')
-            ->getToken()
-            ->getUser()
-        ;
+        $user = $this->getUser();
         var_dump($user->getUsername());die;
 
         $em = $this->getDoctrine()->getManager();
