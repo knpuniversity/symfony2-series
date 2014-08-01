e1588ab364e9bbf1fbfcb5dd50ea4d266a71f56d
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:38:03 -0400

    [tuts-apply-diff] Applying existing diff file for step login-form-create-controller

---
+++ src/Yoda/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Yoda\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+    }
+}
