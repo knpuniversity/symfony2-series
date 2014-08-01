d5248fe3424110bfc6a1ef6038d14b0e7d4c129d
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:38:10 -0400

    [tuts-apply-diff] Applying existing diff file for step save-user-copy-fixtures-class

---
+++ src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Yoda\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadUsers implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+
+    }
+}
