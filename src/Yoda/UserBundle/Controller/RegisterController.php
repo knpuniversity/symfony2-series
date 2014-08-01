4f73e3d3cb04d0715fda80cb5a24e7f20b97eb3d
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:38:21 -0400

    [tuts-apply-diff] Applying existing diff file for step repeated-adding-repeated-field

--- src/Yoda/UserBundle/Controller/RegisterController.php
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -16,8 +16,10 @@ class RegisterController extends Controller
     {
         $form = $this->createFormBuilder()
             ->add('username', 'text')
-            ->add('email', 'text')
-            ->add('password', 'password')
+            ->add('email', 'email')
+            ->add('password', 'repeated', array(
+                'type' => 'password',
+            ))
             ->getForm()
         ;
 
