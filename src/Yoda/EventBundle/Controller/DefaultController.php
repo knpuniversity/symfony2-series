23efcb52ac8a4874d679fb7d86c9c0aa955af98e
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:36:17 -0400

    [tuts-apply-diff] Applying existing diff file for step routing-removing-debug

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -8,8 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($count, $firstName)
     {
-        var_dump($firstName, $count);die;
-
         return $this->render(
             'EventBundle:Default:index.html.twig',
             array('name' => $firstName)
