b440b9d4920e29143e27fc403c06763d583099f3
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:39:20 -0400

    [tuts-apply-diff] Applying existing diff file for step form-class-using-in-the-controller

--- src/Yoda/UserBundle/Controller/RegisterController.php
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Yoda\UserBundle\Entity\User;
+use Yoda\UserBundle\Form\RegisterFormType;
 
 class RegisterController extends Controller
 {
@@ -19,16 +20,7 @@ class RegisterController extends Controller
         $user = new User();
         $user->setUsername('Leia');
 
-        $form = $this->createFormBuilder($user, array(
-            'data_class' => 'Yoda\UserBundle\Entity\User',
-        ))
-            ->add('username', 'text')
-            ->add('email', 'email')
-            ->add('plainPassword', 'repeated', array(
-                'type' => 'password',
-            ))
-            ->getForm()
-        ;
+        $form = $this->createForm(new RegisterFormType(), $user);
 
         $form->handleRequest($request);
         if ($form->isValid()) {
