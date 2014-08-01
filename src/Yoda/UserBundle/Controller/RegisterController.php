176b6bf045747e31d6f31240d7563e5a856cf9b0
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:38:17 -0400

    [tuts-apply-diff] Applying existing diff file for step registration-annotation-routing

--- src/Yoda/UserBundle/Controller/RegisterController.php
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -3,9 +3,13 @@
 namespace Yoda\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class RegisterController extends Controller
 {
+    /**
+     * @Route("/register", name="user_register")
+     */
     public function registerAction()
     {
         // todo
