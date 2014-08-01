8ffb25d593c020d8c0b7bb1f023ef6bc831bb0fa
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:36:15 -0400

    [tuts-apply-diff] Applying existing diff file for step bundles-generate-bundle

---
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Yoda\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render(
+            'EventBundle:Default:index.html.twig',
+            array('name' => $name)
+        );
+    }
+}
