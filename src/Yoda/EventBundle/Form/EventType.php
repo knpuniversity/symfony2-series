26fd4c25f9e652725afd7944ad5ffd7c7eb46d56
Ryan Weaver <ryan@thatsquality.com>
2014-08-01 16:36:22 -0400

    [tuts-apply-diff] Applying existing diff file for step generators-running-the-crud-generator

---
+++ src/Yoda/EventBundle/Form/EventType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Yoda\EventBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EventType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('time')
+            ->add('location')
+            ->add('details')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Yoda\EventBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'yoda_eventbundle_event';
+    }
+}
