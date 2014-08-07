c163d9ff00f63174ffa5d3abd923f4c137f9f946
Ryan Weaver <ryan@thatsquality.com>
2014-08-07 18:21:31 -0400

    [tuts-apply-diff] Applying existing diff file for step csv-create-empty-method-and-route

--- src/Yoda/EventBundle/Controller/ReportController.php
+++ src/Yoda/EventBundle/Controller/ReportController.php
@@ -2,7 +2,15 @@
 
 namespace Yoda\EventBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 class ReportController extends Controller
 {
+    /**
+     * @Route("/events/report/recentlyUpdated.csv")
+     */
+    public function updatedEventsAction()
+    {
 
+    }
 }
