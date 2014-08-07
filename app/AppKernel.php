44225aac80870a21d06ae56c6a0741a988fb462a
Ryan Weaver <ryan@thatsquality.com>
2014-08-07 18:21:18 -0400

    [tuts-apply-diff] Applying existing diff file for step stofextensions-install-the-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Yoda\EventBundle\EventBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Yoda\UserBundle\UserBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
